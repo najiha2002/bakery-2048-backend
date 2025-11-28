e480130e764d209ed8a807c3c9a50b62edecaca2
Najiha <najiha2002@gmail.com>
2025-11-28 19:19:26 +0800

    store enum as numbers

--- Bakery2048/Models/PowerUpType.cs
+++ Bakery2048/Models/PowerUpType.cs
@@ -2,9 +2,9 @@ namespace Bakery2048.Models
 {
     public enum PowerUpType
     {
-        ScoreBoost,
-        TimeExtension,
-        Undo,
-        SwapTiles
+        ScoreBoost = 0,
+        TimeExtension = 1,
+        Undo = 2,
+        SwapTiles = 3
     }
 }
